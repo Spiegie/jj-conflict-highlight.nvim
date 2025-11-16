6dee6add72db9a23698f97943069547d7b318c95
Spiegie <michael.spi@web.de>
2025-11-16 16:47:18 +0100

    test plugin setup

--- conflict-test/conflicted.lua
+++
@@ -1,9 +0,0 @@
-<<<<<<< Conflict 1 of 1
-+++++++ Contents of side #1
-local value = 5 + 7
-print(value)
-print(string.format("value is %d", value))
-%%%%%%% Changes from base to side #2
--local value = 1 + 1
-+local value = 1 - 1
->>>>>>> Conflict 1 of 1 ends
